206cefb38387007652433796a7c5cfb427c5c0c2
Abdullah Al Noman <noman15-5387@diu.edu.bd>
2024-04-27 20:19:03 +0600

    New Spot Add form and many More

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Check-in: (Web-Solution for Hotel Reservation)
+# Mount Ride: (Web-Solution for Tour)
 
 Live Site: 
-# checkin-24.surge.sh
+
 
 # npm packages Use:
 a. AOS Package
@@ -9,13 +9,3 @@ b. Animate.css
 c. Swiper slider
 
 # Key Features:
-1. Real-Time Booking and Availability Checker
-Implement a system that allows users to check the availability of hotels, guesthouses, and restaurant reservations in real-time. This feature should enable instant booking confirmations, making the reservation process seamless and efficient for both the business and the customers.
-2. Interactive Maps and Location Guides
-Use interactive maps to showcase the location of your properties and nearby attractions. This feature can help guests plan their stay more effectively, providing them with a better understanding of what to expect in terms of location and surrounding amenities.
-3. Customer Reviews and Ratings Integration
-Incorporate a section for customer reviews and ratings for each property and dining experience. This transparency builds trust and helps potential guests make informed decisions based on the experiences of others.
-4. Personalized User Accounts and Loyalty Program
-Offer personalized user accounts where guests can manage their bookings, view past stays, and preferences. Integrate a loyalty program that rewards returning customers with discounts, special offers, or perks, encouraging repeat business and fostering a sense of community.
-5. Multi-language Support and Currency Converter
-Given the diverse clientele that the hospitality industry serves, your website should support multiple languages and offer a currency converter. This feature ensures that international guests can easily understand your offerings and make transactions in their currency, enhancing the overall user experience.
--- src/Components/AddTouristSpot.jsx
+++ src/Components/AddTouristSpot.jsx
@@ -1,10 +1,182 @@
 
+
+
 const AddTouristSpot = () => {
-    return (
-        <div>
-            Add Tourist Spot
-        </div>
-    );
+
+
+	const handleAddSpot = e => {
+		e.preventDefault();
+
+		const form = e.target;
+
+		const name = form.name.value;
+		const email = form.email.value;
+		const spot = form.spot.value;
+		const photo = form.photo.value;
+		const country = form.country.value;
+		const location = form.location.value;
+		const description = form.description.value;
+		const cost = form.cost.value;
+		const season = form.season.value;
+		const time = form.time.value;
+		const visitor = form.visitor.value;
+		
+	
+		const newSpot = {name,email,spot,photo,country,location,description,cost,season,time,visitor}
+		console.log(newSpot)
+	}
+
+
+  return (
+    <div>
+      <section className="p-6 bg-yellow-800 text-black">
+        <form
+		onSubmit={handleAddSpot}
+          noValidate=""
+          action=""
+          className="container flex flex-col mx-auto space-y-12 text-black"
+        >
+          <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-gray-50 text-black">
+            <div className="space-y-2 col-span-full lg:col-span-1">
+              <p className="font-bold text-2xl">Add Your Tourist Spot</p>
+              <p className="text-xs">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci fuga autem eum!
+              </p>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 text-black">
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  Your Name 
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="Abdullah Al Noman"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  Your Email 
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Sundarban"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  Spot Name 
+                </label>
+                <input
+                  name="spot"
+                  type="text"
+                  placeholder="Sundarban"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Photo URL 
+                </label>
+                <input
+                  name="photo"
+                  type="text"
+                  placeholder="http//"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Country Name 
+                </label>
+                <input
+                  name="country"
+                  type="text"
+                  placeholder="Bangladesh"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Location 
+                </label>
+                <input
+                  name="location"
+                  type="text"
+                  placeholder="Khulna"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              
+              <div className="col-span-full">
+                <label htmlFor="address" className="text-sm">
+                  Short Description
+                </label>
+                <input
+                  name="description"
+                  type="text"
+                  placeholder="Description" 
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="city" className="text-sm">
+				Average Cost
+                </label>
+                <input
+                  name="cost"
+                  type="text"
+                  placeholder=" Average Cost"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="state" className="text-sm">
+				Season
+                </label>
+                <input
+                  name="season"
+                  type="text"
+                  placeholder="Summer"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="zip" className="text-sm">
+				Travel Time
+                </label>
+                <input
+                  name="time"
+                  type="text"
+                  placeholder="3 Days"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="zip" className="text-sm">
+				Tota Visitors Per Year
+                </label>
+                <input
+                  name="visitor"
+                  type="text"
+                  placeholder="2000"
+                  className="border p-2 w-full rounded-md focus:ring focus:ring-opacity-75  focus:ring-violet-600 border-gray-300"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2 mt-4">
+				<input type="submit" value="Add Spot" className="btn btn-block btn-outline" />
+              </div>
+            </div>
+          </fieldset>
+          
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default AddTouristSpot;
